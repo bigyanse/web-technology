ac0f84a978bb549b577b0a95c604a30d84ffa404
bigyanse <contact.bigyanse@gmail.com>
2023-07-27 21:25:08 +0545

    `add`: cookies homework

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-console.log("Hello");
+function handleError(func) {
+	try {
+		func();
+	} catch(err) {
+		console.log(err);
+	}
+}
 
-const circle = document.querySelector("cirlce");
-circle.style.backgroundColor = "green";
+handleError(() => {
+	const circle = document.querySelector("cirlce");
+	circle.style.backgroundColor = "green";
+});
 
+handleError(() => {
+	document.getElementById("acceptCookies").addEventListener("click", () => {
+		document.cookie = "name=bigyan";
+		document.cookie = "address=koteshwor";
+	});
+});
+
+handleError(() => {
+	document.getElementById("showCookies").addEventListener("click", () => {
+		const cookies = document.cookie.split("; ");
+		const name = cookies[0].split("=")[1];
+		const address = cookies[1].split("=")[1];
+		alert(`Name: ${name}`);
+		alert(`Address: ${address}`);
+	});
+});
